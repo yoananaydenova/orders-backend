a0f843899e5424834b278d06273148c3424de6a5
Yoana Naydenova <y.naydenova@gmail.com>
2024-06-09 22:29:27 +0300

    Initial commit

---
+++ src/main/java/com/yoananaydenova/orders_backend/OrdersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yoananaydenova.orders_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoananaydenova/orders_backend/OrdersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoananaydenova.orders_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
