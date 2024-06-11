e27aa269a4a5d7d1ae42c7a8301b0bbf9f214db7
Yoana Naydenova <y.naydenova@gmail.com>
2024-06-11 17:18:16 +0300

    Add ControllerExceptionHandler

---
+++ src/main/java/com/yoananaydenova/ordersapp/exception/ControllerExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.yoananaydenova.ordersapp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ErrorResponse notFound(NoSuchElementException ex) {
+        return ErrorResponse.create(ex, HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+}
