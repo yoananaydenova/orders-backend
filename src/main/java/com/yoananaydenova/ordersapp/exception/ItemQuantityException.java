9247408df869c0bbfd0d39d2f20c50dee65c090a
Yoana Naydenova <y.naydenova@gmail.com>
2024-06-11 17:17:59 +0300

    Add custom ItemQuantityException

---
+++ src/main/java/com/yoananaydenova/ordersapp/exception/ItemQuantityException.java
@@ -0,0 +1,7 @@
+package com.yoananaydenova.ordersapp.exception;
+
+public class ItemQuantityException extends RuntimeException{
+    public ItemQuantityException(String message) {
+        super(message);
+    }
+}
