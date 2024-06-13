0b854fafa79735339fa7bda83dc2c65d32432c79
Yoana Naydenova <y.naydenova@gmail.com>
2024-06-13 19:25:31 +0300

    Add CrossOrigin annotation in ItemController

--- src/main/java/com/yoananaydenova/ordersapp/controller/ItemController.java
+++ src/main/java/com/yoananaydenova/ordersapp/controller/ItemController.java
@@ -1,18 +1,15 @@
 package com.yoananaydenova.ordersapp.controller;
 
 import com.yoananaydenova.ordersapp.model.Item;
-import com.yoananaydenova.ordersapp.model.Order;
 import com.yoananaydenova.ordersapp.model.dtos.AddItemDTO;
 import com.yoananaydenova.ordersapp.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:5173")
 public class ItemController {
 
     private final ItemService itemService;
