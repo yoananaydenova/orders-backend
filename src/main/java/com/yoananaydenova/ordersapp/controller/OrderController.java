5fb8a4759ab76851d99a82c2dd24eb8bcade2c60
Yoana Naydenova <y.naydenova@gmail.com>
2024-09-26 21:59:47 +0300

    Fix OrderController method return entities to be ResponseEntity

--- src/main/java/com/yoananaydenova/ordersapp/controller/OrderController.java
+++ src/main/java/com/yoananaydenova/ordersapp/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.yoananaydenova.ordersapp.model.dtos.AddOrderDTO;
 import com.yoananaydenova.ordersapp.model.dtos.OrderDTO;
 import com.yoananaydenova.ordersapp.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,29 +22,31 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    // 201 created
     @PostMapping("/order")
-    public OrderDTO createOrder(@RequestBody AddOrderDTO addOrderDTO){
-        return orderService.createOrder(addOrderDTO);
+    public ResponseEntity<OrderDTO> createOrder(@RequestBody AddOrderDTO addOrderDTO){
+        return ResponseEntity.ok(orderService.createOrder(addOrderDTO));
     }
 
-    @GetMapping("/orders")
-    public List<OrderDTO> getAllOrders(){
-        return orderService.getAllOrders();
+    // 200 ok
+    @GetMapping("/all-orders")
+    public ResponseEntity<List<OrderDTO>> getAllOrders(){
+        return ResponseEntity.ok(orderService.getAllOrders());
     }
 
     @GetMapping("/order/{id}")
-    public OrderDTO getSingleOrderById(@PathVariable Long id){
-        return orderService.findById(id);
+    public ResponseEntity<OrderDTO> getSingleOrderById(@PathVariable Long id){
+        return ResponseEntity.ok(orderService.findById(id));
     }
 
     @PutMapping("/order/{id}")
-    public OrderDTO updateOrder(@PathVariable Long id, @RequestBody AddOrderDTO addOrderDTO){
-        return orderService.updateOrderById(id, addOrderDTO);
+    public ResponseEntity<OrderDTO> updateOrder(@PathVariable Long id, @RequestBody AddOrderDTO addOrderDTO){
+        return ResponseEntity.ok(orderService.updateOrderById(id, addOrderDTO));
     }
 
     @DeleteMapping("/order/{id}")
-    public String deleteOrder(@PathVariable Long id){
-        return orderService.deleteOrderById(id);
+    public ResponseEntity<String> deleteOrder(@PathVariable Long id){
+        return ResponseEntity.ok(orderService.deleteOrderById(id));
     }
 
 }
